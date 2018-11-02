7ac58bbbb060283df16d3cb68b76d5fad7afa33f
Cheyra <staiercm@gmail.com>
2018-11-01 21:27:15 -0400

    formupdate

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,2 +1,43 @@
-
-//pollDiv
+var mercuryTotal=0;
+let venusTotal =0;
+let earthTotal =0;
+let marsTotal =0;
+let jupiterTotal=0;
+let saturnTotal =0;
+let uranusTotal =0;
+let neptuneTotal =0
+// $(document).on("click", $("#submit"), function(){
+//     if($('#mercuryPicked').is(':checked')) {
+//         mercuryTotal++
+//         console.log(mercuryTotal)
+//     }
+//     if($('#venusPicked').is(':checked')) {
+//         venusTotal++
+//         console.log(venusTotal)
+//     }
+//     if($('#earthPicked').is(':checked')) {
+//         earthTotal++
+//         console.log(earthTotal)
+//     }
+//     if($('#marsPicked').is(':checked')) {
+//         marsTotal++
+//         console.log(marsTotal)
+//     }
+//     if($('#jupiterPicked').is(':checked')) {
+//         jupiterTotal++
+//         console.log(jupiterTotal)
+//     }
+//     if($('#saturnPicked').is(':checked')) {
+//         saturnTotal++
+//         console.log(saturnTotal)
+//     }
+//     if($('#uranusPicked').is(':checked')) {
+//         uranusTotal++
+//         console.log(uranusTotal)
+//     }
+//     if($('#neptunePicked').is(':checked')) {
+//         neptuneTotal++
+//         console.log(neptuneTotal)
+//     }
+// })
+// //pollDiv
